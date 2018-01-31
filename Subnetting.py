71a66fee40120ae3aa01b433b624236ae3395b37
Jaisaibaba236 <jay.patel76@dal.ca>
2018-01-31 09:54:32 -0400

    Learning classes and obects

--- Subnetting.py
+++ Subnetting.py
@@ -10,16 +10,5 @@ class IPAddressing:
        my_ip_list = self.ip_address.split('.')
        #print(my_ip_list)
 
-
-
 ipaddress = IPAddressing('192.168.10.2')
 ipaddress.subnetting()
-
-def binary(a):
-   s=''
-   t={'0':'000','1':'001','2':'010','3':'011','4':'100','5':'101','6':'110','7':'111'}
-   for c in oct(a)[1:]:
-      s+=t[c]
-   return s
-
-binary(234)
